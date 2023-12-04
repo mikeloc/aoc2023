1dd119d6fff29be0c5b08fe5f25feff2e69b3771
mikeloc <michellocas@hotmail.com>
2023-12-04 13:30:11 -0500

    Day 4

---
+++ Day4_puzzle_1/day4_puzzle1.py
@@ -0,0 +1,24 @@
+#
+# Day 4 Puzzle 1
+#
+
+file = open('Day4_puzzle_1/input2.txt', 'r')
+Lines = file.readlines()
+
+total = 0
+for line in Lines:
+    line = line.rstrip('\n')
+    splitCardIdAndNumbers = line.split(":")
+    splitWinningAndCardNumbers = splitCardIdAndNumbers[1].split("|")
+    winningNumbers = splitWinningAndCardNumbers[0].split()
+    cardNumbers = splitWinningAndCardNumbers[1].split()
+
+    cardPoint = 0 
+    for cardNumber in cardNumbers:
+        if cardNumber in winningNumbers:
+            if cardPoint == 0:
+                cardPoint = 1
+            else:
+                cardPoint = cardPoint * 2
+    total = total + cardPoint
+print("total: ", total )
---
+++ Day4_puzzle_2/day4_puzzle2.py
@@ -0,0 +1,36 @@
+#
+# Day 4 Puzzle 1
+#
+file = open('Day4_puzzle_2/input2.txt', 'r')
+Lines = file.readlines()
+cardData = [0]
+
+
+for line in Lines:
+    line = line.rstrip('\n')
+    splitCardIdAndNumbers = line.split(":")
+    cardId = int(splitCardIdAndNumbers[0].split()[1])
+    cardData.insert(cardId, 1)
+
+total = 0
+for line in Lines:
+    line = line.rstrip('\n')
+    splitCardIdAndNumbers = line.split(":")
+    cardId = int(splitCardIdAndNumbers[0].split()[1])
+    splitWinningAndCardNumbers = splitCardIdAndNumbers[1].split("|")
+    winningNumbers = splitWinningAndCardNumbers[0].split()
+    cardNumbers = splitWinningAndCardNumbers[1].split()
+
+    cardPoint = 0
+    for cardNumber in cardNumbers:
+        if cardNumber in winningNumbers:
+            cardPoint = cardPoint + 1
+    
+    # X cardPoints means the next X card will add the
+    for i in range(cardId+1,cardId+cardPoint+1): 
+        cardData[i] = cardData[i] + cardData[cardId]
+
+for cardPoints in cardData:
+    total = total+cardPoints
+ 
+print("total: ", total )
