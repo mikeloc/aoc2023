949eb6a00b41ca2c8f5d897344a5502b788554da
mikeloc <michellocas@hotmail.com>
2023-12-06 14:26:33 -0500

    Day 6 optimization

--- Day6/day6_puzzle2.py
+++ Day6/day6_puzzle2.py
@@ -4,6 +4,7 @@
 file = open('Day6/input2.txt', 'r')
 Lines = file.readlines()
 
+#Read total times
 time_str = Lines[0].split(':')[1]
 times = time_str.split()
 totalTime = times[0]
@@ -11,6 +12,7 @@ for i in range(1,len(times)):
     totalTime = totalTime+times[i]
 totalTime = int(totalTime)
 
+#Read total distances
 dist_str = Lines[1].split(':')[1]
 distances = dist_str.split()
 totalDistance = distances[0]
@@ -18,18 +20,34 @@ for i in range(1,len(distances)):
     totalDistance = totalDistance + distances[i]
 totalDistance = int(totalDistance)
 
-firstWin = False
-lastWin = False
-win = 0
-for waitTime in range(0, totalTime):
-    if (totalTime-waitTime)*waitTime > totalDistance:
-        if not firstWin:
-            print("firstWin:", waitTime)
-            firstWin=True
-        win = win+1
+def isWinner(waitTime):
+    return(totalTime-waitTime)*waitTime > totalDistance
+
+def binary_search(lower, higher, searchWinner):
+    if (higher-lower) > 1:
+        midway = lower +(higher-lower)//2
+        if isWinner(midway) == searchWinner:
+            return binary_search(lower,midway,searchWinner)
+        else:
+            return binary_search(midway,higher,searchWinner)
     else:
-        if firstWin and not lastWin:
-            print("lastWin", waitTime)
-            lastWin = False
+        return higher
+
+step = totalTime // 10
+firstWin = -1
+lastWin = -1
+
+waitTime=0
+while (waitTime < totalTime):
+    if isWinner(waitTime):
+        if firstWin == -1:
+            firstWin = waitTime
+    else:
+        if firstWin != -1:
+            lastWin = waitTime-step
             break
-print("winning combination",win)
+    waitTime = waitTime + step
+firstWin = binary_search(0, firstWin, searchWinner=True)
+lastWin = binary_search(lastWin,totalTime, searchWinner=False)-1
+print("firstWin:",firstWin,"lastWin:",lastWin)
+print("winning combinations:",lastWin-firstWin+1)
